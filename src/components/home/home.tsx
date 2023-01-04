335c7e66a0b68c669ed8319de0b51558175feed4
Olga <olvstep@mail.ru>
2023-01-04 12:52:39 +0300

    content: add content

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './home.css';
+import { cardsInfo } from '../../data/cards_data';
+import Card from '../card/card';
+import '../../assets/img/1.png';
 
 class Home extends React.Component {
   render() {
     return (
-      <>
-        <h3>Home Page</h3>
-      </>
+      <div className="home">
+        <div className="home__filter">
+          <h3>Filters</h3>
+        </div>
+        <div className="home__main">
+          {cardsInfo.map((card, index) => (
+            <Card {...card} key={index}></Card>
+          ))}
+        </div>
+      </div>
     );
   }
 }
---
+++ src/data/cards_data.ts
@@ -0,0 +1,69 @@
+import img1 from '../assets/img/1.png';
+import img2 from '../assets/img/2.png';
+import img3 from '../assets/img/3.png';
+import img4 from '../assets/img/4.png';
+import img5 from '../assets/img/5.png';
+import img6 from '../assets/img/6.png';
+import img7 from '../assets/img/7.png';
+import img8 from '../assets/img/8.png';
+import img9 from '../assets/img/9.png';
+import img10 from '../assets/img/10.png';
+import img11 from '../assets/img/11.png';
+
+export const cardsInfo: ICard[] = [
+  {
+    src: img1,
+    tags: ['image', 'background'],
+    alt: '1',
+  },
+  {
+    src: img2,
+    tags: ['image', 'background'],
+    alt: '2',
+  },
+  {
+    src: img3,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img4,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img5,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img6,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img7,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img8,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img9,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img10,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+  {
+    src: img11,
+    tags: ['image', 'background'],
+    alt: '3',
+  },
+];

e1176998d77ceace889b38300bed8e2d6481d9f4
Olga <olvstep@mail.ru>
2023-01-22 03:14:47 +0400

    feat: add arrow filter functionality, add color items

--- src/components/filters/filter.tsx
+++ src/components/filters/filter.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './filter.css';
-import { ReactComponent as MarkerSvg } from '../../assets/svg/arrow.svg';
+import { ReactComponent as MarkerSvgUp } from '../../assets/svg/arrow.svg';
+import { ReactComponent as MarkerSvgDown } from '../../assets/svg/arrow-down.svg';
 
 function Filter(props: IFilter) {
+  const [visibility, setToggle] = useState(false);
+
+  const clickArrowHandler = () => {
+    visibility ? setToggle(false) : setToggle(true);
+  };
+
   return (
     <div className="filter">
       <div className="filter__header">
         <div className="filter__icon">{props.icon}</div>
         <div className="filter__name">{props.name}</div>
-        <div className="filter__marker">
-          <MarkerSvg></MarkerSvg>
+        <div className="filter__marker" onClick={() => clickArrowHandler()}>
+          {visibility ? <MarkerSvgDown></MarkerSvgDown> : <MarkerSvgUp></MarkerSvgUp>}
         </div>
       </div>
-      <div className="filter__body">
+      <div className={visibility ? 'filter__body' : 'hidden-filter'}>
         <div className="filter__params">{props.params}</div>
       </div>
     </div>

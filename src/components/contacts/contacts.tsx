ee14a10a5c12e716b07d413ff74d893321fa070c
Olga <olvstep@mail.ru>
2023-02-02 01:48:59 +0400

    feat: add form

--- src/components/contacts/contacts.tsx
+++ src/components/contacts/contacts.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { ContactForm } from '../contactForm/contactForm';
 
 class Contacts extends React.Component {
   render() {
     return (
       <>
         <h3>Contacts</h3>
+        <ContactForm></ContactForm>
       </>
     );
   }
